d4d0626a686493d0ff786c139f5e796852065471
Kieu Huynh <kieuhuynh230506@gmail.com>
2026-06-24 20:01:41 +0700

    fix: cap nhat api link render online

--- src/Service - Ân/api.js
+++ src/Service - Ân/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-// Tự động lấy link online nếu có, nếu không có thì tự quay về cổng 5000 local
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api', 
-});
+// Thay vì dự phòng về localhost:5000, ta dự phòng thẳng về link Render online luôn!
+baseURL: import.meta.env.VITE_API_URL || 'https://blockchain-web-hop-dong-cho-thue.onrender.com/api',});
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token')
